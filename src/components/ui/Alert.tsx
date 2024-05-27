aad9465584493e20dcde80dd982337eae4eb5752
Marwa Mahmoud <mm3764453@gmail.com>
2024-05-27 17:57:30 +0300

    project setup

---
+++ src/components/ui/Alert.tsx
@@ -0,0 +1,10 @@
+/** @format */
+interface IProps {}
+const Alert = ({}: IProps) => { 
+  return (
+    <>
+      <h2>Alert</h2>
+    </>
+  )
+}
+export default Alert
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+interface IProps {}
+const Button = ({}: IProps) => {
+  return (
+    <>
+      <h2>Button</h2>
+    </>
+  )
+}
+export default Button
